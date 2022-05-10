8176b7c3e50a2691523e267a1e4189880fa9aa45
bensonz <mr.bz@hotmail.com>
2022-05-10 22:27:49 +0800

    initialize

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import Cat from "./cat";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> HELLO! </p>
+      </header>
+      <Cat />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cat/index.tsx
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+function Cat() {
+  return <div className="catContainer">cat</div>;
+}
+export default Cat;
